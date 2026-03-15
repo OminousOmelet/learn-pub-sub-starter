aaa878d697a42b51bf4e2a0c6faf1c965e116b19
OminousOmelet <kittenthief@live.com>
2026-03-15 16:27:20 -0400

    connection succesful...

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	const connStr string = "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp091.Dial(connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+	fmt.Println("Connection Sucessful")
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	<-sigChan
+
+	fmt.Println("\nshutting down")
+	os.Exit(0)
 }
